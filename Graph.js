d3d20731dae1aca968d5dc991eed76d2b6b052bd
william <willi.wambu@gmail.com>
2015-11-26 21:22:55 +0300

    changed dfs and bfs, also added topological sort

--- Graph.js
+++ Graph.js
@@ -4,7 +4,7 @@ function Vertex(label) {
 }
 
 /**
- * A graph implemeted using adjacency list
+ * A graph implemented using adjacency list
  * @param v is the number of vertices
  * @constructor
  */
@@ -12,11 +12,10 @@ function Graph(v) {
     this.vertices = v;
     this.edges = 0;
     this.adj = [];
+    this.vertexList = []; //store the vertex details, like course name, for Topological Sort
 
     for (var i = 0; i < this.vertices; ++i) {
         this.adj[i] = [];
-        //fill the adj list with empty strings equal to the number of vertices
-        this.adj[i].push("");
     }
 
     this.addEdge = addEdge;
@@ -25,17 +24,19 @@ function Graph(v) {
     this.bfs = bfs;
     this.pathTo = pathTo;
     this.hasPathTo = hasPathTo;
+    this.topSortHelper = topSortHelper;
+    this.topSort = topSort;
     //this.toString = toString;
 
     //array to store visited nodes
     this.marked = [];
 
     //initialize it false values equal to # of vertices
-    for (var i = 0; i < this.vertices; ++i) {
+    for (var i = 0; i < this.vertices; i++) {
         this.marked[i] = false;
     }
 
-    this.edgeTo = [];
+    this.edgeTo = new Array();
 }
 
 function addEdge(v, w) {
@@ -50,23 +51,27 @@ function addEdge(v, w) {
 }
 
 function showGraph() {
-    //loop through all vertices
+    var visited = [];
+    var str = '';
     for (var i = 0; i < this.vertices; ++i) {
-        console.log(i + " -> ");
-
+        str += this.vertexList[i] + " -> ";
+        visited.push(this.vertexList[i]);
         for (var j = 0; j < this.vertices; ++j) {
             if (this.adj[i][j] != undefined) {
-                console.log(this.adj[i][j] + ' ');
+                if (visited.indexOf(this.vertexList[j]) < 0) {
+                    str += this.vertexList[j] + ' ';
+                }
             }
         }
-
-        //newline separator
-        console.log()
+        console.log(str);
+        visited.pop();
     }
 }
 
-//Depth First Search Algorithm
-//v is where to start the search from
+/**
+ * Depth First Search Algorithm
+ * v is where to start the search from
+ * */
 function dfs(v) {
     //mark the vertex as visited
     this.marked[v] = true;
@@ -76,9 +81,11 @@ function dfs(v) {
         console.log("Visited vertex " + v);
     }
 
-    for (var w in this.adj[v]) {
-        if (!this.marked[w]) {
-            this.dfs(w);
+    for (var i = 0; i < this.adj[v].length; i++) {
+        console.log(this.adj[v]);
+        var element = this.adj[v][i];
+        if (!this.marked[element]) {
+            this.dfs(element);
         }
     }
 }
@@ -89,14 +96,16 @@ function bfs(s) {
     queue.push(s); // add to back of queue
     while (queue.length > 0) {
         var v = queue.shift(); // remove from front of queue
-        if (v == undefined) {
+        if (v != undefined) {
             console.log("Visited vertex: " + v);
         }
-        for (var w in this.adj[v]) {
-            if (!this.marked[w]) {
-                this.edgeTo[w] = v;
-                this.marked[w] = true;
-                queue.push(w);
+
+        for (var i = 0; i < this.adj[v].length; i++) {
+            var element = this.adj[v][i];
+            if (!this.marked[element]) {
+                this.edgeTo[element] = v;
+                this.marked[element] = true;
+                queue.push(element);
             }
         }
     }
@@ -115,21 +124,71 @@ function pathTo(v) {
     for (var i = v; i != source; i = this.edgeTo[i]) {
         path.push(i);
     }
-    path.push(s);
+    path.push(source);
     return path;
 }
 
+function topSort(){
+    var stack = [];
+    var visited = [];
+
+    //initializing visited array to false
+    for(var i=0;i < this.vertices; i++){
+        visited[i] = false;
+    }
+
+    for(var i=0;i < this.vertices; i++){
+        if(visited[i] == false){
+            this.topSortHelper(i,visited,stack);
+        }
+    }
+
+    for(var i = 0; i < stack.length; i++){
+        if(stack[i] != undefined && stack[i] != false){
+            console.log(this.vertexList[stack[i]]);
+        }
+    }
+
+}
+
+function topSortHelper(v,visited,stack){
+    visited[v] = true;
+
+    for(var i = 0; i < this.adj[v]; i++){
+        var element = this.adj[v][i];
+        if(!visited[element]){
+            this.topSortHelper(visited[element],visited,stack);
+        }
+    }
+    stack.push(v);
+}
+/*
 g = new Graph(5);
 g.addEdge(0, 1);
 g.addEdge(0, 2);
 g.addEdge(1, 3);
 g.addEdge(2, 4);
-var vertex = 4;
+g.bfs(0);
+var vertex = 3;
 var paths = g.pathTo(vertex);
+var str = '';
 while (paths.length > 0) {
     if (paths.length > 1) {
-        console.log(paths.pop() + '-');
+       str += (paths.pop() + '-');
     } else {
-        console.log(paths.pop());
+        str += paths.pop();
     }
 }
+console.log(str);*/
+
+g = new Graph(6);
+g.addEdge(1,2);
+g.addEdge(2,5);
+g.addEdge(1,3);
+g.addEdge(1,4);
+g.addEdge(0,1);
+g.vertexList = ["CS1", "CS2", "Data Structures",
+    "Assembly Language", "Operating Systems",
+    "Algorithms"];
+g.showGraph();
+g.topSort();
