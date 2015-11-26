cf953432d4ab31f9ebb16e49088bb49159e08770
william <willi.wambu@gmail.com>
2015-11-26 21:26:56 +0300

    Code reformat

--- Graph.js
+++ Graph.js
@@ -128,65 +128,65 @@ function pathTo(v) {
     return path;
 }
 
-function topSort(){
+function topSort() {
     var stack = [];
     var visited = [];
 
     //initializing visited array to false
-    for(var i=0;i < this.vertices; i++){
+    for (var i = 0; i < this.vertices; i++) {
         visited[i] = false;
     }
 
-    for(var i=0;i < this.vertices; i++){
-        if(visited[i] == false){
-            this.topSortHelper(i,visited,stack);
+    for (var i = 0; i < this.vertices; i++) {
+        if (visited[i] == false) {
+            this.topSortHelper(i, visited, stack);
         }
     }
 
-    for(var i = 0; i < stack.length; i++){
-        if(stack[i] != undefined && stack[i] != false){
+    for (var i = 0; i < stack.length; i++) {
+        if (stack[i] != undefined && stack[i] != false) {
             console.log(this.vertexList[stack[i]]);
         }
     }
 
 }
 
-function topSortHelper(v,visited,stack){
+function topSortHelper(v, visited, stack) {
     visited[v] = true;
 
-    for(var i = 0; i < this.adj[v]; i++){
+    for (var i = 0; i < this.adj[v]; i++) {
         var element = this.adj[v][i];
-        if(!visited[element]){
-            this.topSortHelper(visited[element],visited,stack);
+        if (!visited[element]) {
+            this.topSortHelper(visited[element], visited, stack);
         }
     }
     stack.push(v);
 }
 /*
-g = new Graph(5);
-g.addEdge(0, 1);
-g.addEdge(0, 2);
-g.addEdge(1, 3);
-g.addEdge(2, 4);
-g.bfs(0);
-var vertex = 3;
-var paths = g.pathTo(vertex);
-var str = '';
-while (paths.length > 0) {
-    if (paths.length > 1) {
-       str += (paths.pop() + '-');
-    } else {
-        str += paths.pop();
-    }
-}
-console.log(str);*/
+ g = new Graph(5);
+ g.addEdge(0, 1);
+ g.addEdge(0, 2);
+ g.addEdge(1, 3);
+ g.addEdge(2, 4);
+ g.bfs(0);
+ var vertex = 3;
+ var paths = g.pathTo(vertex);
+ var str = '';
+ while (paths.length > 0) {
+ if (paths.length > 1) {
+ str += (paths.pop() + '-');
+ } else {
+ str += paths.pop();
+ }
+ }
+ console.log(str);*/
 
 g = new Graph(6);
-g.addEdge(1,2);
-g.addEdge(2,5);
-g.addEdge(1,3);
-g.addEdge(1,4);
-g.addEdge(0,1);
+g.addEdge(1, 2);
+g.addEdge(2, 5);
+g.addEdge(1, 3);
+g.addEdge(1, 4);
+g.addEdge(0, 1);
 g.vertexList = ["CS1", "CS2", "Data Structures",
     "Assembly Language", "Operating Systems",
     "Algorithms"];
